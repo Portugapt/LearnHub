79cbe8d8f600f2a149356c56bcedfe292c5a414c
Portugapt <monteiro.joao.ps@gmail.com>
2022-07-07 20:40:20 +0100

    redis mount volume right

--- Courses/Linkedin: Become a NoSQL Developer/4- Redis/start-redis-server.sh
+++ Courses/Linkedin: Become a NoSQL Developer/4- Redis/start-redis-server.sh
@@ -1,1 +1,1 @@
-docker run -it --name redisContainer -v /local-data/:/data -p 6379:6379 redis/redis-stack-server:latest
+docker run -it --name redisContainer -v `pwd`/local-data/:/data -p 6379:6379 redis/redis-stack-server:latest
